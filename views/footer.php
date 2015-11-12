f5b4921ab20bd714878b2fae958097ccb7a865af
siemdicou <siemdicou@hotmail.com>
2015-11-12 09:49:56 +0100

    h

--- views/footer.php
+++ views/footer.php
@@ -1,4 +1,7 @@
 <footer>
-	&COPY; Joey en Siem 
-
+	&COPY; Joey en Siem  
+	<div class="footerButton">gebruiksvoorwaarden </div>
+	<div class="footerButton">privacy</div> 
+	<div class="footerButton">contact</div> 
+	<div class="footerButton">adverteren</div> 
 </footer>
