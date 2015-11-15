f4da5be40504571769384a4efe8371d7eda56a92
joey1006 <joeysteffens@quicknet.nl>
2015-11-16 00:00:15 +0100

    wat gekloot met de header maar de style werkt echniet mee....

--- views/header.php
+++ views/header.php
@@ -1,4 +1,5 @@
 <div id="header">
-	<div id="logo"></div>
-
+	<div id="logo"><img src="img/logo.png"></div>
+    <div id="radio-number-search"><img src="img/placeholder.jpg"> </div>
+    <div id="live-music"><img src="img/placeholder.jpg"> </div>
 </div>
