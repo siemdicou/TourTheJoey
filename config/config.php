0fad040b302130778eb406688330c444dc88a558
siemdicou <siemdicou@hotmail.com>
2015-11-16 20:09:18 +0100

    xz

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+// Databasegegevens
+const DB_HOST = '127.0.0.1';
+const DB_NAME = 'DB2352607';
+const DB_USERNAME = 'U2352607';
+const DB_PASSWORD = 'ddypjcrh28';
--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php 
+	require_once 'config/config.php';
+	require_once 'includes/database.php';
 	include 'views/head.php';
 	include 'views/header.php';
 	include 'views/body.php';
 	// include 'views/body-sidebar.php';
-
 	include 'views/footer.php';
+	$result = $mysqli->query("SELECT * FROM users");
+    // while($pictures=$result->fetch_assoc()){
+    
  ?>
