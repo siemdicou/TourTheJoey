8adfbe54027614f10a2837f3100ea893d762039b
joey1006 <joeysteffens@quicknet.nl>
2015-11-17 13:58:44 +0100

    hallo

--- views/top100.php
+++ views/top100.php
@@ -3,9 +3,11 @@
 
 ?>
 <html>
-<p>1</p>
-<p></p>
-<p></p>
-<p></p>
-<p></p>
+<div id="liedjeswrap">
+<h1>1</h1><p>liede een met znger en andere info</p><br>
+<h1>2</h1><p>liede een met znger en andere info</p><br>
+<h1>3</h1><p>liede een met znger en andere info</p><br>
+<h1>4</h1><p>liede een met znger en andere info</p><br>
+<h1>5</h1><p>liede een met znger en andere info</p><br>
+</div>
 </html>
