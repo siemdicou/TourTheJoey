a5e91dc06f0d999b33453cec15d030a2760d165e
joey1006 <joeysteffens@quicknet.nl>
2015-11-17 11:44:07 +0100

    \

--- views/body.php
+++ views/body.php
@@ -6,8 +6,18 @@
         $page = (empty($_GET['page'])) ? '' : $_GET['page'];
 
         switch($page){
-            case
-
+            case top100:
+                include'top100.php';
+                break;
+//            case prijsvraag:
+//                include'';
+//                break;
+//            case ppp:
+//                include'';
+//                break;
+//            case pp:
+//                include'';
+//                break;
         }
 			include 'views/body-content.php';
 
