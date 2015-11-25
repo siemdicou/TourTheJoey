41dde37f80de2424eef2886f64aeec55be56965e
siemdicou <siemdicou@hotmail.com>
2015-11-25 15:15:33 +0100

    saf

--- views/header.php
+++ views/header.php
@@ -2,20 +2,14 @@
 
 	<div id="logo"><img src="img/logo.png"></div>
     <div id="radio-number-search">
-<<<<<<< Updated upstream
         <h1>tour top 100</h1>
     <img src="img/placeholder.jpg">
     </div>
     <div id="live-music"><img src="img/placeholder.jpg"> </div>
     <div id="fiets"><img src="img/fiets.png"></div>
     <img src="img/placeholder.jpg" id="padgination">
-=======
-    	<h1>tour top 100</h1>
- 		<img src="img/placeholder.jpg">
-     </div>
-    <div id="live-music"><img src="img/placeholder.jpg"> </div>
 
 
->>>>>>> Stashed changes
+
 
 </div>
