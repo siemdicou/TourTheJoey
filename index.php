183a0edfc2e40ebbf8eefca6c644c3741cdc0431
siemdicou <siemdicou@hotmail.com>
2015-11-30 15:54:17 +0100

    hi

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 	include 'config/config.php';
 	require_once 'includes/database.php';
 	include 'views/head.php';
+	include 'views/sub-nav.html';
 	include 'views/header.php';
 	include 'views/padgination.php';
 	include 'views/body.php';
--- views/header.php
+++ views/header.php
@@ -1,16 +1,4 @@
-<div id="sub-nav">
-	<table>
-		<tr>
-			<td>nieuwsbrief</td>
-			<td>mobiel</td>
-			<td>contact</td>
-			<td>meld een fout</td>
-			<td>frequenties</td>
-			<td>help</td>
-			<td>rss</td>
-		</tr>
-	</table>
-</div>
+
 <div id="header">	
 	<div id="logo"><img src="img/logo.png"></div>
     <div id="radio-number-search">
