9ac2ad5ac887db20d9c2405cab0520be39d4962c
siemdicou <siemdicou@hotmail.com>
2015-11-30 11:25:50 +0100

    fra

--- views/header.php
+++ views/header.php
@@ -2,15 +2,11 @@
 
 	<div id="logo"><img src="img/logo.png"></div>
     <div id="radio-number-search">
-        <h1>tour top 100</h1>
-    <img src="img/placeholder.jpg">
+        <img src="img/payoff.png" >
+    <img src="img/placeholder.jpg" id="radio-number-search2">
     </div>
     <div id="live-music"><img src="img/placeholder.jpg"> </div>
     <div id="fiets"><img src="img/fiets.png"></div>
     
     <!-- <img src="img/placeholder.jpg" id="padgination"> -->
-
-
-
-
 </div>
