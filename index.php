5a6bdaca556f79b6808765cafe0434e5594b3843
siemdicou <siemdicou@hotmail.com>
2015-12-02 10:55:13 +0100

    hi

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <?php 
 	include 'config/config.php';
 	require_once 'includes/database.php';
@@ -5,7 +6,41 @@
 	include 'views/sub-nav.html';
 	include 'views/header.php';
 	include 'views/padgination.php';
-	include 'views/body.php';
+?>
+<div id="wrapper">
+
+	<div id="content">
+	<?php
+
+        $page = (empty($_GET['page'])) ? '' : $_GET['page'];
+        $id = (empty($_GET['id'])) ? '' : $_GET['id'];
+        switch($page){
+            case 'top':
+                include'views/top100.php';
+         	break;
+         	case 'detail':
+                include'views/detail.php';
+         	break;
+
+         	default:
+         		include 'views/home.php';
+         	break;	
+         	};
+
+			
+		 ?>
+
+	</div>
+
+	<div id="sidebar">
+		<?php
+			include 'views/body-sidebar.php';
+		 ?>
+
+	 </div>
+</div>
+<?php
 	include 'views/footer.php';
-    
- ?>
+?>
+
+
--- views/detail.php
+++ views/detail.php
@@ -1,8 +1,55 @@
-w<form method="POST">
+<?php 
+	$result = $mysqli->query("SELECT * FROM liedjes WHERE id='".$_GET["id"]."'");
+    $songs=$result->fetch_assoc();
+    {	
+    	// echo "<a href=".$id-1.">";
+    	echo $id-1;
+    	echo "<br>";
+    	// echo "</a>";
+    	echo $songs['numbers'];
+    	echo $songs['song' ];
+    	echo "<br>";
+    }
+	
+    $songs2=$result->fetch_assoc();
+    {
+    	echo "<div id='liedjeswrap'>";
+        echo "<div class='numbers'>";
+        echo "<p>";
+    	echo $songs['numbers'];
+        echo "</div>";
+        echo "<div id='songs'>";
+        echo "<b>";
+  		echo $songs['song'];
+        echo "</b>";
+        echo "</div>";
+        echo "</p>";
+  		echo "</div>";
+  		echo "<br>";
+    }
+    	
+        $songs3=$result->fetch_assoc();
+    {	
+    	echo $id+1;
+    	echo "<br>";
+    	echo $songs['numbers'];
+    	echo $songs['song'];
+
+    	echo "<br>";
+   
+    }
+
+     ?>
+
+
+
+
+
+<form method="POST">
 <p>naam</p>
 <input type="text">
 
-<p>email</p>
+<p>email (wordt niet getoond)</p>
 <input type="email">
 <p>website</p>
 <input type="text">
