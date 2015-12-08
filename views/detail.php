cc7f4b09bcc7d0fd93323a9622db7c54f53150b3
siemdicou <siemdicou@hotmail.com>
2015-12-08 11:29:56 +0100

    saf

--- views/detail.php
+++ views/detail.php
@@ -14,10 +14,23 @@
 			    	echo "</a>";
 			    	echo "<br>";
 
+
 			 
 	   	}
     
-	   	$current_id=$previous_id+1;
+        $next_id=$previous_id+2;
+	   	$result = $mysqli->query("SELECT * FROM songs WHERE id='".$next_id."'");
+	   	while ($songs=$result->fetch_assoc()) {
+			    	echo "<a href='index.php?page=detail&id=".$next_id." '>";
+			    	echo $songs['numbers'];
+			    	echo " ";
+			    	echo utf8_encode($songs['song_title']);
+			    	echo "<br>";
+			    	echo "</a>";
+			    	echo "<br>";
+	   	}
+
+	   	$current_id=$next_id-1;
 	   	$result = $mysqli->query("SELECT * FROM songs WHERE id='".$current_id."'");
 	    $songs=$result->fetch_assoc();
 	    {
@@ -36,24 +49,23 @@
 	        echo "</b>";
 	        echo "</div>";
 	        echo "</p>";
-	  		echo "</div>";
-	  		echo "<iframe src='https://www.youtube.com/embed/".$songs['youtube-url']."'></iframe>";
+	        echo "<iframe src='https://www.youtube.com/embed/".$songs['youtube-url']."'></iframe>";
 	  		echo "<br>";
+	  		echo "<br>";
+	  		echo "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+	  		
+	  		echo "<br>";
+	  		echo "</div>";
 
 			
 	    }
  
-    	$next_id=$current_id+1;
-	   	$result = $mysqli->query("SELECT * FROM songs WHERE id='".$next_id."'");
-	   	while ($songs=$result->fetch_assoc()) {
-			    	echo "<a href='index.php?page=detail&id=".$next_id." '>";
-			    	echo $songs['numbers'];
-			    	echo " ";
-			    	echo utf8_encode($songs['song_title']);
-			    	echo "<br>";
-			    	echo "</a>";
-			    	echo "<br>";
-	   	}
+
 
      ?>
 
