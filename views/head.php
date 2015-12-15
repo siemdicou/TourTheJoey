f9ba4fa4408e9b9bc2a046d415dea59786d66330
joey1006 <joeysteffens@quicknet.nl>
2015-12-15 12:08:18 +0100

    k

--- views/head.php
+++ views/head.php
@@ -1,12 +1,3 @@
 <!--/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>links naar de csss files<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/-->
-<!-- <link rel="stylesheet" type="text/css" href="css/main.css">
-<link rel="stylesheet" type="text/css" href="css/home.css">
-<link rel="stylesheet" type="text/css" href="css/footer.css">
-<link rel="stylesheet" type="text/css" href="css/header.css">
-<link rel="stylesheet" type="text/css" href="css/prijsvraag.css">
-<link rel="stylesheet" type="text/css" href="css/detailpagina.css">
-<link rel="stylesheet" type="text/css" href="css/top100.css">
-<link rel="stylesheet" type="text/css" href="css/top100wrapper.css">
-<link rel="stylesheet" type="text/css" href="css/bn.css"> -->
-<link rel="stylesheet" type="text/css" href="css/main.scss">
+<link rel="stylesheet" type="text/css" href="css/main.css">
 <meta charset="UTF-8">
