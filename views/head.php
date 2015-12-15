568ab77b165d322765f102cf1727a8010e92eee9
siemdicou <siemdicou@hotmail.com>
2015-12-15 09:49:09 +0100

    sfa

--- views/head.php
+++ views/head.php
@@ -1,6 +1,6 @@
 <!--/*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>links naar de csss files<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/-->
-<link rel="stylesheet" type="text/css" href="css/home.css">
 <link rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" type="text/css" href="css/home.css">
 <link rel="stylesheet" type="text/css" href="css/footer.css">
 <link rel="stylesheet" type="text/css" href="css/header.css">
 <link rel="stylesheet" type="text/css" href="css/prijsvraag.css">
